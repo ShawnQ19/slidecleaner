071af1be1db565c0908b1adc7342bdf45a9c20ed
ShawnQ <q1743@ShawnQ.com>
2026-06-24 16:12:26 +0800

    v1.7.3: test auto-build hook

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 26
         targetSdk = 35
         // 语义化版本：主版本.次版本.修订号
-        versionCode = 10702
-        versionName = "1.7.2"
+        versionCode = 10703
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
